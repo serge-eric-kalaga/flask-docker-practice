6a6f7414c4750ed0a1dd9cec0c5fdfd754b89aae
serge-eric-kalaga <kalagaserge4@gmail.com>
2022-12-14 08:42:19 +0000

    refractoring du projet

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask 
+from flask import Flask , jsonify
 from app import run_app
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = run_app()
 
-from app.routes import *
 
+
+@app.route('/')
+def index():
+    u = []
+    with app.app_context():
+        from app.models.models import User
+        users = User.getAll()
+        jsn = [{"id": u.id, "username": u.username} for u in users]
+    return jsn or []
 
 
 if __name__ == '__main__':
--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,13 @@ def run_app(config=AppConfig):
     app = Flask(__name__)
 
     app.config.from_object(AppConfig)
-
+    
+    from app.models import db
+    
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
 
     return app
 
